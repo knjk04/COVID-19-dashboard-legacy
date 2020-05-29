c0221f1594e8f4994fe2b7695c83208651ca7a57
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-05-29 08:23:59 +0100

    add constructors

--- src/main/java/com/karankumar/covid19stats/ui/views/country/CountryView.java
+++ src/main/java/com/karankumar/covid19stats/ui/views/country/CountryView.java
@@ -1,7 +1,11 @@
 package com.karankumar.covid19stats.ui.views.country;
 
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 
 @PageTitle("Country")
-public class CountryView {
+public class CountryView extends VerticalLayout {
+    public CountryView() {
+
+    }
 }
--- src/main/java/com/karankumar/covid19stats/ui/views/global/GlobalView.java
+++ src/main/java/com/karankumar/covid19stats/ui/views/global/GlobalView.java
@@ -1,7 +1,14 @@
 package com.karankumar.covid19stats.ui.views.global;
 
+import com.karankumar.covid19stats.ui.MainView;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 
+@Route(value = "global", layout = MainView.class)
 @PageTitle("Global")
-public class GlobalView {
+public class GlobalView extends VerticalLayout {
+    public GlobalView() {
+
+    }
 }
