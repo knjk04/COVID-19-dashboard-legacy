d42e542831fa07a83836578f61bcc18135531702
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-05-30 15:22:18 +0100

    add grouping separators

--- src/main/java/com/karankumar/covid19dashboard/ui/views/global/GlobalView.java
+++ src/main/java/com/karankumar/covid19dashboard/ui/views/global/GlobalView.java
@@ -10,20 +10,28 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import java.text.NumberFormat;
+
 @Route(value = "global", layout = MainView.class)
 @PageTitle("COVID-19: Global statistics")
 public class GlobalView extends VerticalLayout {
     public GlobalView() {
         GlobalStats globalStats = new GlobalStats();
+        int totalDeaths = globalStats.getTotalDeaths();
+        int totalRecovered = globalStats.getTotalRecovered();
+        int totalCases = globalStats.getTotalCases();
 
-        System.out.println("GlobalView: Total deaths: " + globalStats.getTotalDeaths());
-        System.out.println("GlobalView: Total recovered: " + globalStats.getTotalRecovered());
-        System.out.println("GlobalView: Total cases: " + globalStats.getTotalCases());
+        System.out.println("GlobalView: Total deaths: " + totalDeaths);
+        System.out.println("GlobalView: Total recovered: " + totalRecovered);
+        System.out.println("GlobalView: Total cases: " + totalCases);
+
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        numberFormat.setGroupingUsed(true);
 
         Board board = new Board();
-        Div totalDeathsDiv = createComponent("Total deaths: " + globalStats.getTotalDeaths());
-        Div totalRecoveredDiv = createComponent("Total recovered: " + globalStats.getTotalRecovered());
-        Div totalCasesDiv = createComponent("Total cases: " + globalStats.getTotalCases());
+        Div totalDeathsDiv = createComponent("Total deaths: " + numberFormat.format(totalDeaths));
+        Div totalRecoveredDiv = createComponent("Total recovered: " + numberFormat.format(totalRecovered));
+        Div totalCasesDiv = createComponent("Total confirmed cases: " + numberFormat.format(totalCases));
         board.addRow(totalDeathsDiv, totalRecoveredDiv, totalCasesDiv);
 
         add(board);
