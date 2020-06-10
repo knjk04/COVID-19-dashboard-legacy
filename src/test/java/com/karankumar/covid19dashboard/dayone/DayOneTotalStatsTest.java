4ebc8b6a9366df455d76dff2a156ad189019da47
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-06-10 17:03:29 +0100

    add country summary json

--- src/test/java/com/karankumar/covid19dashboard/dayone/DayOneTotalStatsTest.java
+++ src/test/java/com/karankumar/covid19dashboard/dayone/DayOneTotalStatsTest.java
@@ -1,4 +1,14 @@
 package com.karankumar.covid19dashboard.dayone;
 
+import com.karankumar.covid19dashboard.testutils.TestUtil;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
+
 public class DayOneTotalStatsTest {
+    private static JSONObject json;
+
+    @BeforeAll
+    public static void setupData() {
+        json = TestUtil.readJSON(TestUtil.COUNTRY_SUMMARY_FILE_PATH);
+    }
 }
