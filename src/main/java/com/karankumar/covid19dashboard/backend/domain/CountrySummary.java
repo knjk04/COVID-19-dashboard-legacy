14e7eab77691305e09eed0b484c1a4db1e023692
Karan <karan@karankumar.com>
2020-12-21 19:51:55 +0000

    Use Lombok in CountrySummary

--- src/main/java/com/karankumar/covid19dashboard/backend/domain/CountrySummary.java
+++ src/main/java/com/karankumar/covid19dashboard/backend/domain/CountrySummary.java
@@ -1,5 +1,10 @@
 package com.karankumar.covid19dashboard.backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class CountrySummary {
     private final String countryName;
     private final int totalDeaths;
@@ -8,43 +13,4 @@ public class CountrySummary {
     private final int totalNewCases;
     private final int totalNewDeaths;
     private final int totalNewRecovered;
-
-    public CountrySummary(String countryName, int totalDeaths, int totalRecovered, int totalConfirmedCases,
-                          int totalNewCases, int totalNewDeaths, int totalNewRecovered) {
-        this.countryName = countryName;
-        this.totalDeaths = totalDeaths;
-        this.totalRecovered = totalRecovered;
-        this.totalConfirmedCases = totalConfirmedCases;
-        this.totalNewCases = totalNewCases;
-        this.totalNewDeaths = totalNewDeaths;
-        this.totalNewRecovered = totalNewRecovered;
-    }
-
-    public String getCountryName() {
-        return countryName;
-    }
-
-    public int getTotalDeaths() {
-        return totalDeaths;
-    }
-
-    public int getTotalRecovered() {
-        return totalRecovered;
-    }
-
-    public int getTotalConfirmedCases() {
-        return totalConfirmedCases;
-    }
-
-    public int getTotalNewCases() {
-        return totalNewCases;
-    }
-
-    public int getTotalNewDeaths() {
-        return totalNewDeaths;
-    }
-
-    public int getTotalNewRecovered() {
-        return totalNewRecovered;
-    }
 }
