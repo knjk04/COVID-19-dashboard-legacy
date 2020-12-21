4697fc8aea744e380749e13a473a8e898bbbb7ff
Karan <karan@karankumar.com>
2020-12-21 20:26:39 +0000

    Refactor tests

--- src/test/java/com/karankumar/covid19dashboard/backend/api/summary/SummaryStatsTest.java
+++ src/test/java/com/karankumar/covid19dashboard/backend/api/summary/SummaryStatsTest.java
@@ -7,19 +7,21 @@ import com.karankumar.covid19dashboard.testutils.TestUtil;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 import java.util.TreeMap;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assumptions.assumeThat;
+
 class SummaryStatsTest {
     private static JSONObject jsonObject;
     private static SummaryStats summaryStats;
 
     @BeforeAll
-    public static void setupData() {
+    public static void setUp() {
         jsonObject = TestUtil.readJson(TestUtil.GLOBAL_SUMMARY_FILE_PATH);
         summaryStats = new SummaryStats();
         try {
@@ -28,8 +30,7 @@ class SummaryStatsTest {
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-        // only run the tests if the json object is not null
-        Assumptions.assumeTrue(jsonObject != null);
+        assumeThat(jsonObject).isNotNull();
     }
 
     /**
@@ -38,25 +39,25 @@ class SummaryStatsTest {
     @Test
     void timeCorrectlyFormatted() {
         String timeGroundTruth = "12:26:39";
-        Assertions.assertEquals(summaryStats.getTime(), timeGroundTruth);
+        assertThat(summaryStats.getTime()).isEqualTo(timeGroundTruth);
     }
 
     @Test
     void checkTotalRecovered() {
         Integer totalRecoveredGroundTruth = 3_000_504;
-        Assertions.assertEquals(summaryStats.getTotalRecovered(), totalRecoveredGroundTruth);
+        assertThat(totalRecoveredGroundTruth).isEqualTo(summaryStats.getTotalRecovered());
     }
 
     @Test
     void checkTotalConfirmedCases() {
         Integer totalConfirmedCasesGroundTruth = 6_829_314;
-        Assertions.assertEquals(summaryStats.getTotalConfirmedCases(), totalConfirmedCasesGroundTruth);
+        assertThat(totalConfirmedCasesGroundTruth).isEqualTo(summaryStats.getTotalConfirmedCases());
     }
 
     @Test
     void checkTotalDeaths() {
         Integer totalDeathsGroundTruth = 402_636;
-        Assertions.assertEquals(summaryStats.getTotalDeaths(), totalDeathsGroundTruth);
+        assertThat(summaryStats.getTotalDeaths()).isEqualTo(totalDeathsGroundTruth);
     }
 
     @Test
@@ -81,6 +82,7 @@ class SummaryStatsTest {
 
     @Test
     void mostCasesCorrectlySet() {
+        // given
         TreeMap<Integer, String> mostCasesGroundTruth = new TreeMap<>();
         mostCasesGroundTruth.put(1_897_380, CountryName.UNITED_STATES_OF_AMERICA.toString());
         mostCasesGroundTruth.put(614_941, CountryName.BRAZIL.toString());
@@ -88,17 +90,27 @@ class SummaryStatsTest {
         mostCasesGroundTruth.put(284_734, CountryName.UNITED_KINGDOM.toString());
         mostCasesGroundTruth.put(240_978, CountryName.SPAIN.toString());
 
-        Assertions.assertEquals(summaryStats.getMostCases(), mostCasesGroundTruth);
+        // when
+        TreeMap<Integer, String> actual = summaryStats.getMostCases();
+
+        // then
+        assertThat(actual).isEqualTo(mostCasesGroundTruth);
     }
 
     @Test
     void mostDeathsCorrectlySet() {
+        // given
         TreeMap<Integer, String> mostDeathsGroundTruth = new TreeMap<>();
         mostDeathsGroundTruth.put(109_132, CountryName.UNITED_STATES_OF_AMERICA.toString());
         mostDeathsGroundTruth.put(40_344, CountryName.UNITED_KINGDOM.toString());
         mostDeathsGroundTruth.put(34_021, CountryName.BRAZIL.toString());
         mostDeathsGroundTruth.put(33_774, CountryName.ITALY.toString());
         mostDeathsGroundTruth.put(29_114, CountryName.FRANCE.toString());
-        Assertions.assertEquals(summaryStats.getMostDeaths(), mostDeathsGroundTruth);
+
+        // when
+        TreeMap<Integer, String> actual =summaryStats.getMostDeaths();
+
+        // then
+        assertThat(actual).isEqualTo(mostDeathsGroundTruth);
     }
 }
--- src/test/java/com/karankumar/covid19dashboard/backend/utils/UtilTest.java
+++ src/test/java/com/karankumar/covid19dashboard/backend/utils/UtilTest.java
@@ -3,22 +3,29 @@ package com.karankumar.covid19dashboard.backend.utils;
 import com.karankumar.covid19dashboard.backend.api.summary.SummaryConst;
 import com.karankumar.covid19dashboard.testutils.TestUtil;
 import org.json.JSONObject;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtilTest {
     private JSONObject jsonObject;
 
     @Before
-    public void setupData() {
+    public void setUp() {
         jsonObject = TestUtil.readJson(TestUtil.GLOBAL_SUMMARY_FILE_PATH);
     }
 
     // The date should be formatted as dd/MM/yyyy
     @Test
     public void dateCorrectlyFormatted() {
+        String expected = "06-06-2020";
         String date = jsonObject.getString(SummaryConst.DATE);
-        Assert.assertEquals("06-06-2020", DateUtil.formatDate(date));
+
+        // when
+        String actual = DateUtil.formatDate(date);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
